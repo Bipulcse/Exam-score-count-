13518143a4022284ebffe1f7035867febe07b9cc
Tasnif00 <65866575+Tasnif00@users.noreply.github.com>
2022-11-20 12:12:15 +0600

    exam score count
    exam score count with python

---
+++ exam.py
@@ -0,0 +1,138 @@
+b1=int(input("Input Your Marks in Bangla 1st Paper: "))
+b2=int(input("Input Your Marks in Bangla 2nd Paper: "))
+e1=int(input("Input Your Marks in English 1st Paper: "))
+e2=int(input("Input Your Marks in English 2nd Paper: "))
+math=int(input("Input Your Marks in Math: "))
+Hmath=int(input("Input Your Marks in Higher Math Subject: "))
+Hmathp=int(input("Input Your Marks in Higher Math Pratical: "))
+bio=int(input("Input Your Marks in Biology Subject: "))
+biop=int(input("Input Your Mark in Biology Pratical: "))
+phy=int(input("Input Your Marks in Physics Subject: "))
+phyp=int(input("Input Your Marks in Physics Pratical: "))
+che=int(input("Input Your Marks in Chemistry Subject: "))
+chep=int(input("Input Your Marks in Chemistry Pratical: "))
+islam=int(input("Input Your Marks in Islam: "))
+Sscince=int(input("Input Your Marks in Social Science: "))
+
+#**********************************************************
+#**********************************************************
+
+totalbangla=b1+b2
+totalEnglish=e1+e2
+totalHmath=Hmath+Hmathp
+totalBiology=bio+biop
+totalPhysics=phy+phyp
+totalChemistry=che+chep
+#*********************************
+#*********************************
+
+Total=totalbangla+totalEnglish+totalHmath+totalChemistry+totalBiology+totalPhysics+math+islam+Sscince
+
+avg=Total/9
+
+
+#**************************
+#**************************
+
+Print("*************************")
+Print("*************************")
+
+print("Bangla 1st paper Grade is= ",b1)
+print("Bangla 2nd paper Grade is= ",b2)
+print("English 1st paper Grade is= ",e1)
+print("English 2nd paper Grade is= ",e2)
+print("Math Grade is= ",math)
+print("Higher Math Subject Grade is= ",Hmath)
+print("Higher Math Pratical Grade is= ",Hmathp)
+print("Biology Subject Grade is= ",bio)
+print("Biology Pratical Grade is= ",biop)
+print("Physics Subject Grade is= ",phy)
+print("Physics Pratical Grade is= ",phyp)
+print("Chemistry Subject Grade is= ",che)
+print("Chemistry Pratical Grade is= ",chep)
+print("Islam Subject Grade is= ",islam)
+print("Social Science Subject Grade is= ",Sscince)
+Print("*************************")
+Print("*************************")
+print("Overall Bangla= ",totalbangla)
+print("Overall English= ",totalEnglish)
+
+#*****************************
+#*****************************
+
+Print("**************************")
+Print("**************************")
+
+if b1<33:
+    print("You are Failed in Bangla 1st paper")
+elif b2<33:
+    print("You are Failed in Bangla 2nd paper")
+elif e1<33:
+    print("Your are Failed in English 1st paper")
+elif e2<33:
+    print("You are Failed in English 2nd paper")
+elif math<33:
+    print("You are failed in Math")
+elif Hmath<24.75:
+    print("You are failed in Higher math Subject")
+elif Hmathp<8.25:
+    print("You are Failed in Higher math Pratical")
+elif bio<24.75:
+    print("You are Failed in Biology Subject")
+elif biop<8.25:
+    print("You are Failed in Biology Pratical")
+elif phy<24.75:
+    print("You are Failed in Physics Subject")
+elif phyp<8.25:
+    print("You are Failed in Physics Pratical")
+elif che<24.75:
+    print("You are Failed in Chemistry Subject")
+elif chep<8.25:
+    print("You are Failed in Chemistry Pratical")
+elif islam<33:
+    print("You are Failed in Islam")
+elif Sscince<33:
+    print("You are Failed in Islam")
+else:
+    print("Invalid Code")
+
+Print("**************************")
+Print("**************************")
+
+if avg>80 and avg<100:
+    print("Your Grade is A+")
+elif avg>70 and avg<79:
+    print("Your Grade is A")
+elif avg>60 and avg<69:
+    print("Your Grade Is A-")
+elif avg>50 and avg<59:
+    print("Your Grade is B")
+elif avg>40 and avg<49:
+    print("Your Grade is C")
+elif avg>33 and avg<39:
+    print("Your Grade is D")
+elif avg<33:
+    print("Your Grand is F")
+else:
+    print("Invalid Grading")
+
+
+Print("**************************")
+Print("**************************")
+
+if avg>80 and avg<100:
+    print("Your  Point is GPA- 5 ")
+elif avg>70 and avg<79:
+    print("Your Point is GPA- 4" )
+elif avg>60 and avg<69:
+    print("Your  Point is GPA- 3.5")
+elif avg>50 and avg<59:
+    print("Your Point is GPA- 3 ")
+elif avg>40 and avg<49:
+    print("Your Point is GPA- 2 ")
+elif avg>33 and avg<39:
+    print("Your Point is GPA- 1 ")
+elif avg<33:
+    print("Your Point is GPA- 0 ")
+else:
+    print("Invalid Pointing")
